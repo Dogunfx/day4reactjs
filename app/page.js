84aa80c402b7d7746060d038b510e6085db1f69b
dogunfx <dogunfx@gmail.com>
2024-04-19 12:42:56 +0100

    made modification

--- app/page.js
+++ app/page.js
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+    <main>
+      <div className="header">
+        <div className="icon">
+          <i className="fa fa-bars"></i>
+        </div>
+        <h1 className="no-space title">
+          English Premier League Teams by DogunFX
+        </h1>
+        <div className="whatsapp">
+          <a target="_blank" href="https://wa.me/+2348059060890">
+            <i className="fa fa-whatsapp"></i>
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="teams">
+        <div className="row">
+          <SingleTeam
+            teamName="Man City"
+            teamDesc="Treble Winner for 2023 Season"
+            teamClass="mancity"
+            teamURL="https://www.mancity.com"
+            teamLogo="images/mancity.png"
+          />
+          <SingleTeam
+            teamName="Arsenal"
+            teamDesc="Young Team star"
+            teamClass="arsenal"
+            teamLogo="images/Arsenal_FC.svg"
+            teamURL="https://arsenal.com"
+          />
+          <SingleTeam
+            teamName="Arsenal"
+            teamDesc="Young Team star"
+            teamClass="arsenal"
+            teamLogo="images/Arsenal_FC.svg"
+            teamURL="https://arsenal.com"
+          />
+        </div>
       </div>
     </main>
+  );
+}
+
+function SingleTeam({ teamLogo, teamName, teamDesc, teamClass, teamURL }) {
+  return (
+    <div className="team">
+      <div className="team-thumbnail ">
+        <img src={teamLogo} alt="" />
+      </div>
+      <h4>{teamName}</h4>
+      <p className="italics">{teamDesc}</p>
+      <a className={teamClass} href={teamURL} target="_blank">
+        View More
+      </a>
+    </div>
   );
 }
